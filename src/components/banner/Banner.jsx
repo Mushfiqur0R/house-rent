84bdfb0480d664ef0a0be8847cff8fa491f7ac4a
Mushfiqur0R <musfikur.dev@gmail.com>
2024-04-18 21:47:44 +0600

    update banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -7,24 +7,41 @@ import banner5 from '../../assets/banner5.jpg';
 import banner6 from '../../assets/banner6.jpg';
 
 import banner4 from '../../assets/banner4.avif';
+
 const Banner = () => {
   return (
-
-      <Swiper
+    <Swiper
       spaceBetween={50}
       slidesPerView={2}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
+      breakpoints={{
+        // Breakpoints for responsive design
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+      }}
     >
-      <SwiperSlide><img className='w-full h-96' src={banner1} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='w-full h-96' src={banner2} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='w-full h-96' src={banner5} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='w-full h-96' src={banner4} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='w-full h-96' src={banner3} alt="" /></SwiperSlide>
-      <SwiperSlide><img className='w-full h-96' src={banner6} alt="" /></SwiperSlide>
-     
+      <SwiperSlide>
+        <img className='w-full h-96' src={banner1} alt="Luxury Villa with Ocean View" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className='w-full h-96' src={banner2} alt="Modern Condo in Downtown" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className='w-full h-96' src={banner5} alt="Luxury Penthouse with City Views" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className='w-full h-96' src={banner4} alt="Spacious Family Home" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className='w-full h-96' src={banner3} alt="Cozy Cottage by the Lake" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className='w-full h-96' src={banner6} alt="Modern Loft in Historic Building" />
+      </SwiperSlide>
     </Swiper>
-   
   );
 };
 
